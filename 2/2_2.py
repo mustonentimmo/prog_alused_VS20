ee0fa8115fd7c88738dfe75abef3b6734e4378bf
Timmo <timmo.mustonen@khk.com>
2020-10-08 10:51:54 +0300

    ÜL 1 (Tingimuslause if)

--- 2/2_2.py
+++ 2/2_2.py
@@ -2,6 +2,8 @@ perenimi = input("Sisestage Leedu perenimi: ")
 if perenimi[-2:] == "ne":
     print("abielus")
 elif perenimi[-2:] == "te":
-    print("määramata")
+    print("vallaline")
 elif perenimi[-1] == "e":
-    print("ilmselt ei ole leedulanna perekonnanimi")
+    print("määramata")
+elif perenimi[-1] != "e":
+    print("Ei ole leedulanna")
---
+++ 2/3_2 ÜL.py
@@ -0,0 +1,5 @@
+vanus = int(input("Sisesta oma vanus: "))
+sugu = input("Sisestage oma sugu: ")
+treeningutüüp = input("treeningu tüübi (1 - tervisetreening, 2 - põhivastupidavuse treening, 3 - intensiivne aeroobne treening)")
+
+def sugu():
