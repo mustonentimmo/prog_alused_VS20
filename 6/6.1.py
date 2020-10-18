d5730c3210a20c82526460353d37448e63cf1a54
timmo <mustonentimmo@gmail.com>
2020-10-18 22:20:58 +0300

    ÜL 6.1

---
+++ 6/6.1.py
@@ -0,0 +1,12 @@
+reklaamlause1 = input("Sisesta reklaamlause ")
+
+def advertise(reklaamlause1):
+    print(reklaamlause1)
+
+advertise(reklaamlause1)
+
+korduste_arv = int(input("Sisesta korduste arv "))
+reklaamlause2 = input("Sisesta reklaamlause ")
+
+for x in range(korduste_arv):
+    print(reklaamlause2)
